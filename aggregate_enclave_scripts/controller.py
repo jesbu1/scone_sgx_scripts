0328923c84a6de3ffebcc9d54155fef368c24faf
jesbu1 <jessezhang5@gmail.com>
2018-11-13 21:03:29 -0800

    changed path to correct one for microsoft server

--- aggregate_enclave_scripts/controller.py
+++ aggregate_enclave_scripts/controller.py
@@ -28,9 +28,11 @@ def query_start():
 	3. Ask for query from them
 	"""
 	list_of_containers = ["18f729d9838a4e8ab66c3a6aac2ecdb0", "28f729d9838a4e8ab66c3a6aac2ecdb0", "38f729d9838a4e8ab66c3a6aac2ecdb0"] #open()
-	path = '/Users/Jesse/Desktop/e-mission/e-mission-server/'
+	#path = '/Users/Jesse/Desktop/e-mission/e-mission-server/'
+	path = '/home/jessez/e-mission-server/'
 	ret = {}
 	print(request.data)
+	mount = Mount(target='/usr/src/app/conf/storage/db.conf', source='"$(pwd)"/conf/storage/db.conf', type='bind')
 	for container in list_of_containers:
 		ret[container] = client.containers.run('skxu3/emission-scone3.5', ['SCONE_MODE=AUTO', 'python user_enclave_scripts.py ' + str(request.data) + ' ' + container],
 		 name=container, remove=True, network='e-mission', ports={'8080':8080}, mounts=[mount], volumes={path :{'bind':'/usr/src/myapp','mode':'rw'}}, working_dir='/usr/src/myapp', detach=False)
