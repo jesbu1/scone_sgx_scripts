4f325a19a2f2a0eccf7c83dcd881090e661185dc
Jesse Zhang <jessezhang5@gmail.com>
2018-11-19 20:51:58 -0800

    implemented multithreading for controller logic

--- aggregate_enclave_scripts/controller.py
+++ aggregate_enclave_scripts/controller.py
@@ -15,7 +15,17 @@ client = docker.from_env()
 
 app = Flask(__name__)
 
-
+class DockerThread(threading.Thread):
+	def __init__(self, image, query_type, container, initial_command, aggregator, mount):
+		self.image = image
+		self.query_type = query_type
+		self.container = container
+		self.initial_command = initial_command
+		self.aggregator = aggregator
+		self.mount = mount
+    def run(self):
+    	client.containers.run(self.image, command= self.initial_command + self.query_type + ' ' + self.container + ' ' +  self.aggregator,
+    		name = self.container, remove=True, network='e-mission', ports = {'8080':8080}, mounts=[self.mount], volumes={path :{'bind':'/usr/src/myapp','mode':'rw'}}, working_dir='/usr/src/myapp')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -35,17 +45,16 @@ def query_start():
 	list_of_containers = ["18f729d9838a4e8ab66c3a6aac2ecdb0", "28f729d9838a4e8ab66c3a6aac2ecdb0", "38f729d9838a4e8ab66c3a6aac2ecdb0"] #open()
 	#path = '/Users/Jesse/Desktop/e-mission/e-mission-server/'
 	path = '~/e-mission-server'
-	ret = {}
-        query_type = str(request.data, 'utf-8')
+    query_type = str(request.data, 'utf-8')
 	print(query_type)
 	mount = Mount(target='/usr/src/app/conf/storage/db.conf', source= path + 'conf/storage/db.conf', type='bind')
+	threads = []
 	for container in list_of_containers:
-		ret[container] = client.containers.run('skxu3/emission-scone3.5', command='bash bash_file ' + query_type + ' ' + container + ' 35.236.79.116:80',
-		 name=container, remove=True, network='e-mission', ports={'8080':8080}, mounts=[mount], volumes={path :{'bind':'/usr/src/myapp','mode':'rw'}}, working_dir='/usr/src/myapp', detach=False)
-		#ret[container] = client.containers.run('skxu3/emission-scone3.5', ['/bin/bash','python3 aggregate_enclave_scripts/user_enclave_scripts.py ' + str(request.data) + ' ' + container],
-		 #name=container, remove=True, network='e-mission', ports={'8080':8080}, mounts=[mount], volumes={path :{'bind':'/usr/src/myapp','mode':'rw'}}, working_dir='/usr/src/myapp', detach=False)
-		print(ret)
-		return json.dumps(ret)
-	
+		thread = DockerThread('skxu3/emission-scone3.5', query_type, container, 'bash bash_file', '35.236.79.116:80', mount)
+		threads.append(thread)
+		thread.start()
+	for thread in threads:
+		thread.join()
+	return "Finished"	
 if __name__ == "__main__":
 	app.run(port=80, debug=True)
