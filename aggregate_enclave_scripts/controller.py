198cf79b77d918c3bcc203e01bca998edafc9ce7
Jesse Zhang <jessezhang5@gmail.com>
2018-12-02 17:02:47 -0800

    fix bug

--- aggregate_enclave_scripts/controller.py
+++ aggregate_enclave_scripts/controller.py
@@ -62,8 +62,9 @@ if __name__ == "__main__":
 	mount = Mount(target='/usr/src/app/conf/storage/db.conf', source= path + 'conf/storage/db.conf', type='bind')
 	for i in range(len(list_of_containers)):
 		container = list_of_containers[i]
+		print(container)
 		list_of_containers[i] = [client.containers.run('skxu3/emission-scone3.5', command = "bash bash_file",
 			name = container, remove=True, network='e-mission', mounts=[mount], volumes={path :{'bind':'/usr/src/myapp','mode':'rw'}}, working_dir='/usr/src/myapp', detach=True),
 			container]
-		list_of_containers[i].pause()
+		list_of_containers[i][0].pause()
 	print(list_of_containers)
